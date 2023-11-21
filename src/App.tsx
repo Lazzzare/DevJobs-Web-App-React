d648fed716c8a2f5fba261f3b17ab96a7a3e476d
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-21 20:46:03 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import Header from "./components/Header";
+import { useState } from "react";
+import Input from "./components/Input";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
   return (
-    <div className="w-full min-h-screen">
-      <Header />
+    <div
+      className={`w-full min-h-screen ${
+        darkMode ? "bg-Midnight" : "bg-LightGrey"
+      } duration-1000 transition-all`}
+    >
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Input />
     </div>
   );
 };
