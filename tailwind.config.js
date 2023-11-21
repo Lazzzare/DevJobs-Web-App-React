009cfeece8ea07f80112fee2ab9da557942154fb
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-21 20:02:14 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Violet: "#9e7f66",
+        LightViolet: "#939BF4",
+        VeryDarkBlue: "#19202D",
+        Midnight: "#121721",
+        White: "#FFFFFF",
+        LightGrey: "#F4F6F8",
+        Gray: "#9DAEC2",
+        DarkGrey: "#6E8098",
+      },
+      backgroundImage: {
+        MobileHeader: "url('/src/assets/mobile/bg-pattern-header.svg')",
+      },
+    },
   },
   plugins: [],
 };
