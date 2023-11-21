abef58eec69fb0192381af55c78117f2e1eee2e4
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-21 19:52:15 +0400

    install react and ts with vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div className="w-full min-h-screen">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
