5fa3a14c9bfbe39c8f0665dfe2fddc662c339a13
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-11-27 20:41:13 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import Header from "./components/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Input from "./components/Input";
+import axios from "axios";
+import dataType from "./components/dataTypes";
+import Jobs from "./components/Jobs";
+
+const baseUrl = "./../data/data.json";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState<boolean>(false);
+  const [data, setData] = useState<(typeof dataType)[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(baseUrl);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div
       className={`w-full min-h-screen ${
@@ -12,6 +32,7 @@ const App = () => {
     >
       <Header darkMode={darkMode} setDarkMode={setDarkMode} />
       <Input darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Jobs data={data} />
     </div>
   );
 };
