90c2933903b9385dccbc899a402010691c83a482
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-08 22:54:02 +0400

    Update Jobs.tsx

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -6,16 +6,26 @@ interface Props {
 
 const Jobs = ({ data }: Props) => {
   return (
-    <div className="px-6 mt-14">
-      <div>
-        {data.map((job) => {
-          return (
-            <div key={job.id}>
-              <img src={job.logo} alt={`${job.company} Logo`} />
-            </div>
-          );
-        })}
-      </div>
+    <div className="px-6 mt-14 flex flex-col space-y-11">
+      {data.map((job) => {
+        return (
+          <div key={job.id} className="relative pt-12 pb-9 px-8 bg-white">
+            <img
+              src={job.logo}
+              alt={`${job.company} Logo`}
+              className="absolute -top-5 left-8"
+            />
+            <p className="text-DarkGrey">
+              {job.postedAt} <span>.</span> {job.contract}
+            </p>
+            <h1 className="mt-4 mb-[17px] text-VeryDarkBlue text-xl font-bold">
+              {job.position}
+            </h1>
+            <h2 className="text-DarkGrey">{job.company}</h2>
+            <p className="mt-11 text-Blue text-sm font-bold">{job.location}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
