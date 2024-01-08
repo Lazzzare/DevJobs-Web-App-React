ac2dbdc27d7694e3939b2093c87d74ea25e403fc
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-08 22:57:57 +0400

    Update Jobs.tsx

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -2,14 +2,20 @@ import dataType from "./../components/dataTypes";
 
 interface Props {
   data: (typeof dataType)[];
+  darkMode: boolean;
 }
 
-const Jobs = ({ data }: Props) => {
+const Jobs = ({ data, darkMode }: Props) => {
   return (
     <div className="px-6 mt-14 flex flex-col space-y-11">
       {data.map((job) => {
         return (
-          <div key={job.id} className="relative pt-12 pb-9 px-8 bg-white">
+          <div
+            key={job.id}
+            className={`relative pt-12 pb-9 px-8 ${
+              darkMode ? "bg-VeryDarkBlue" : "bg-white"
+            } duration-1000 transition-all`}
+          >
             <img
               src={job.logo}
               alt={`${job.company} Logo`}
@@ -18,7 +24,11 @@ const Jobs = ({ data }: Props) => {
             <p className="text-DarkGrey">
               {job.postedAt} <span>.</span> {job.contract}
             </p>
-            <h1 className="mt-4 mb-[17px] text-VeryDarkBlue text-xl font-bold">
+            <h1
+              className={`mt-4 mb-[17px] ${
+                darkMode ? "text-white" : "text-VeryDarkBlue"
+              } text-xl font-bold`}
+            >
               {job.position}
             </h1>
             <h2 className="text-DarkGrey">{job.company}</h2>
