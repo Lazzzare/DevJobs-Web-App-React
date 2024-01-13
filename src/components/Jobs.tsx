79db91420f8aa9aa961c11bd9f4f4865f332550d
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2024-01-14 00:06:30 +0400

    Update Jobs.tsx

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -1,4 +1,6 @@
 import dataType from "./../components/dataTypes";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 interface Props {
   data: (typeof dataType)[];
@@ -10,30 +12,40 @@ const Jobs = ({ data, darkMode }: Props) => {
     <div className="mt-14 mx-auto grid-flow-row max-w-[327px] md:max-w-[689px] lg:max-w-[1110px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {data.map((job) => {
         return (
-          <div
+          <motion.div
+            initial={{ opacity: 0, y: 150 }}
+            animate={{ opacity: 6, y: 0 }}
+            transition={{ duration: 1 }}
             key={job.id}
-            className={`relative pt-12 pb-9 px-8 max-w-[327px] md:max-w-[339px] lg:max-w-[350px] mb-10 ${
-              darkMode ? "bg-VeryDarkBlue" : "bg-white"
-            } duration-1000 transition-all`}
           >
-            <img
-              src={job.logo}
-              alt={`${job.company} Logo`}
-              className="absolute -top-5 left-8"
-            />
-            <p className="text-DarkGrey">
-              {job.postedAt} <span>.</span> {job.contract}
-            </p>
-            <h1
-              className={`mt-4 mb-[17px] ${
-                darkMode ? "text-white" : "text-VeryDarkBlue"
-              } text-xl font-bold`}
-            >
-              {job.position}
-            </h1>
-            <h2 className="text-DarkGrey">{job.company}</h2>
-            <p className="mt-11 text-Blue text-sm font-bold">{job.location}</p>
-          </div>
+            <Link to={`/${job.id}`}>
+              <div
+                className={`relative pt-12 pb-9 px-8 max-w-[327px] md:max-w-[339px] lg:max-w-[350px] mb-10 ${
+                  darkMode ? "bg-VeryDarkBlue" : "bg-white"
+                } duration-1000 transition-all hover:scale-105 hover:bg-slate-200`}
+              >
+                <img
+                  src={job.logo}
+                  alt={`${job.company} Logo`}
+                  className="absolute -top-5 left-8"
+                />
+                <p className="text-DarkGrey">
+                  {job.postedAt} <span>.</span> {job.contract}
+                </p>
+                <h1
+                  className={`mt-4 mb-[17px] ${
+                    darkMode ? "text-white" : "text-VeryDarkBlue"
+                  } text-xl font-bold`}
+                >
+                  {job.position}
+                </h1>
+                <h2 className="text-DarkGrey">{job.company}</h2>
+                <p className="mt-11 text-Blue text-sm font-bold">
+                  {job.location}
+                </p>
+              </div>
+            </Link>
+          </motion.div>
         );
       })}
     </div>
